f66a1035d5b9208c4b6dab94c9736e4e51166dd3
Alexandra <al.s.bondarenko@gmail.com>
2014-08-18 15:51:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Downloader::Application.config.secret_key_base = '03fbd300afb1601a7e6a081052bdc30e67adc9853dd8290dd8ae8cf18360ef26061b8ae273ebd4462846cf5dd16e3f10431ef1b4ddbc3dc8dc51382d89bbee0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Downloader::Application.config.session_store :cookie_store, key: '_Downloader_session'
