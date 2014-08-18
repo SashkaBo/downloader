497bb8a584b07c946446ff2b54518e1264f79442
Alexandra <al.s.bondarenko@gmail.com>
2014-08-18 23:48:17 +0300

    Add ability to upload files

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,2 +1,35 @@
 class DocsController < ApplicationController
+
+  def index
+    @docs = []
+    User.each { |u| @docs += u.docs }
+  end
+
+  def create
+    respond_to do |format|
+      if params[:user][:files]
+        params[:user][:files].each { |file| current_user.docs.create(attachment: file) }
+        format.html { redirect_to docs_path, notice: 'New file was successfully added.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to users_path, alert: "New file wasn't added!" }
+        format.json { head :no_content }
+      end
+    end
+  end
+
+  def destroy
+    @doc = User.find(params[:user_id]).docs.find(params[:id])
+    @doc.destroy
+    respond_to do |format|
+      format.html { redirect_to docs_url, notice: 'File was deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def doc_params
+    params.require(:doc).permit(:attachment)
+  end
+
 end
